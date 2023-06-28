42b6df1d9990b1708b2ea9427e806317dce584dc
Jessica Carrillo <jessymamo@gmail.com>
2023-06-27 20:46:35 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+   
+  if (ride <= 400) {
+    return "This one is on me!"
+  }
+  if((ride > 400) && (ride < 2000)) {
+    return 'That will be twenty bucks.';
+  }
+  if ((ride >= 2000) && (ride < 2500)){
+    return 'I will gladly take your thirty bucks.';
+  }
+  if (ride >= 2500)
+    return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const result = city === "NYC" ? 'Ok, sounds good.' : 'No go.'
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+ 
 }
+
+
+
+
+
+
+
